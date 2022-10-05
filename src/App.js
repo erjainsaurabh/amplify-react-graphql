023ff2c530f1b3fb910e12b35de1b586cb3f81a2
Saurabh Jain <saurabh.y.jain@accenture.com>
2022-10-05 11:20:28 -0400

    update again

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App({ signOut }) {
     <View className="App">
       <Card>
         <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth--Saurabh12121!</Heading>
+        <Heading level={1}>We now have Authorization!</Heading>
       </Card>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
